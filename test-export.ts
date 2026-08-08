22182f790ce42033ad09328e0ee41dd24c59bf21
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2026-08-08 15:33:09 +0530

    Remove unused test files for PNG export functionality

--- test-export.ts
+++
@@ -1,5 +0,0 @@
-import { toPng } from 'html-to-image';
-
-// Just to check if it compiles
-const el = document.getElementById('svg-mat') as unknown as HTMLElement;
-toPng(el).then(console.log);
--- test-export2.ts
+++
@@ -1,2 +0,0 @@
-import { toPng } from 'html-to-image';
-toPng(document.body, { width: 100, height: 100, pixelRatio: 2 });
