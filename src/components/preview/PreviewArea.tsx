3f3ab52cb76b9e4deb5bc32049a25bb66fbb27c2
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2026-08-16 20:39:25 +0530

    style: add gap to Canvas Area for improved layout consistency in PreviewArea

--- src/components/preview/PreviewArea.tsx
+++ src/components/preview/PreviewArea.tsx
@@ -7,14 +7,14 @@ export function PreviewArea() {
   return (
     <div className="relative w-full h-full flex flex-col bg-background/50 overflow-hidden">
       {/* Canvas Area */}
-      <div className="flex-1 w-full h-full flex flex-col items-center justify-center pt-6 lg:pt-24 pb-12 px-4 lg:px-16 bg-[url('/grid.svg')] bg-center relative">
-        <div className="lg:absolute lg:top-6 lg:left-1/2 lg:-translate-x-1/2 z-50 w-full flex justify-center lg:w-auto mb-4 lg:mb-0 shrink-0">
+      <div className="flex-1 w-full h-full flex flex-col items-center justify-center pt-6 lg:pt-24 pb-12 px-4 lg:px-16 bg-[url('/grid.svg')] bg-center relative gap-4 lg:gap-0">
+        <div className="lg:absolute lg:top-6 lg:left-1/2 lg:-translate-x-1/2 z-50 w-full flex justify-center lg:w-auto shrink-0">
           <TopToolbar />
         </div>
 
         <div
           id="svg-export-container"
-          className="w-full flex-1 min-h-0 flex items-center justify-center bg-transparent rounded-xl"
+          className="w-full lg:flex-1 min-h-0 flex items-center justify-center bg-transparent rounded-xl"
         >
           <SvgMat />
         </div>
